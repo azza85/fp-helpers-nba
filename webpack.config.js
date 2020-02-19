786cc28449bcd8a38a33dc64390c0edf968f1fac
aaronk85 <aaronkenez@gmail.com>
2020-02-19 21:08:46 +1100

    add in config files

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const config = {
+  entry: {
+    'my-lib': './src/index.ts',
+    'my-lib.min': './src/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, '_bundles'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'MyLib',
+    umdNamedDefine: true
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: true,
+      include: /\.min\.js$/
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.tsx?$/,
+      loader: 'awesome-typescript-loader',
+      exclude: /node_modules/,
+      query: {
+        declaration: false
+      }
+    }]
+  }
+}
+module.exports = config
